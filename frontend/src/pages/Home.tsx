c83f83fab235c70852bfe2a02f8b77e1435248c5
Rob McQueen <robmcqn@gmail.com>
2021-01-29 17:10:39 -0500

    index by sentences instead

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { Pane, SearchInput, Heading, majorScale, Button } from 'evergreen-ui'
+import { Pane, SearchInput, Heading, majorScale, Button, Dialog } from 'evergreen-ui'
 import Data from "../data/human-condition.json"
 import Highlighter from "react-highlight-words";
 import Fuse from "fuse.js"
+import "./Home.css"
 
-interface Reference {
-  text: string
-  page: number
-}
-
-type SearchResult = Fuse.FuseResult<Reference>[]
+type SearchResult = Fuse.FuseResult<"">
 
 export default () => {
   const [query, setQuery] = useState("")
-  const [results, setResults] = useState<SearchResult>([])
+  const [results, setResults] = useState<SearchResult[]>([])
+  const [selected, setSelected] = useState<SearchResult | undefined>(undefined)
+  const [modalShown, setModalShown] = useState<boolean>(false)
 
   const options = {
-    includeScore: true,
-    shouldSort: true,
+    shouldSort: false,
     useExtendedSearch: true,
     keys: ['text']
   }
   const fuse = new Fuse(Data, options)
+
+  useEffect(() => {
+    if (selected === undefined) return
+    setModalShown(true)
+  }, [selected])
 
   /* useEffect(() => {
    *   setResults(fuse.search<Reference>("'freedom"))
    * }, []) */
 
   const handleSubmit = () => {
-    setResults(fuse.search<Reference>(`'${query}`))
+    setResults(fuse.search<"">(`'${query}`))
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,11 +41,20 @@ export default () => {
     if (e.keyCode === 13) handleSubmit()
   }
 
+  let selectedText = ""
+  if (selected) {
+    selectedText = Data.slice(selected.refIndex - 2, selected.refIndex + 2).join(". ")
+  }
+
   return (
     <Pane display="flex" flexDirection="column">
+      <Dialog hasFooter={false} hasHeader={false} isShown={modalShown} onCloseComplete={() => setModalShown(false)}>
+        {selected &&
+          <Highlighter searchWords={[selected.item]} textToHighlight={selectedText} />}
+      </Dialog>
 
       {/** Header **/}
-      <Pane height="300px" display="flex" background="gray">
+      <Pane height="300px" display="flex" background="gray" marginBottom={majorScale(2)}>
         <Pane margin="auto" display="flex" flexDirection="column" minWidth="500px" alignItems="center">
           <Heading size={900} padding={majorScale(3)}>Hannah Arendt Index</Heading>
           <Pane display="flex" alignItems="center">
@@ -54,16 +65,16 @@ export default () => {
       </Pane>
 
       {/** Body **/}
+
       <Pane display="flex" flexDirection="column" alignItems="center">
+        {results.length > 0 && <Pane width={majorScale(64)} display="flex" justifyContent="flex-end">
+          Results: {results.length}
+        </Pane>
+        }
         {results.map((r, i) => (
-          <Pane elevation={0} width={majorScale(64)} margin={majorScale(2)} key={`${r.item.page}${i}`}>
-            <Pane height={majorScale(4)} borderBottom display="flex" alignItems="center" >
-              <Pane marginLeft={majorScale(1)}>
-                <b>Page:</b> {r.item.page}
-              </Pane>
-            </Pane>
+          <Pane elevation={0} hoverElevation={1} width={majorScale(64)} margin={majorScale(2)} key={`${r.refIndex}${i}`} onClick={() => setSelected(r)} className="Clickable">
             <Pane padding={majorScale(2)}>
-              <Highlighter searchWords={[query]} textToHighlight={r.item.text} />
+              <Highlighter searchWords={[query]} textToHighlight={r.item} />
             </Pane>
           </Pane>
         ))}
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -2,40 +2,23 @@ import json
 
 
 def process_file():
-    pages = []
-    current_page = ""
-    page_num = 1
+    full_text = ""
     for line in open("./data/human-condition.txt", "r"):
-        line = line.strip().rstrip()
-        if line.startswith("[") and line.endswith("]"):
-            pages.append({
-                'page': page_num,
-                'text': current_page
-            })
-            current_page = ""
-            page_num += 1
+        line = line.strip().rstrip()        
+        if line.startswith("["):
             continue
-
         if line == "The Human Condition":
             continue
-
         if not line:
             continue
 
-        current_page += " " + line
+        full_text += " " + line
 
-    if current_page:
-        pages.append({'page': page_num, 'text': current_page})
 
-    # For Elasticsearch Bulk API
-    # with open("./build/human-condition.json", "w") as f:
-    #     for p in pages:
-    #         f.write(json.dumps({"index":{"_id":"%s" % p['page']}}) + "\n")
-    #         f.write(json.dumps(p) + "\n")
-
+    sentences = full_text.split(".")
     # For JSON
     with open("./build/human-condition.json", "w") as f:
-        json.dump(pages, f)
+        json.dump(sentences, f)
 
 if __name__ == "__main__":
     process_file()
